a795123df4e160e818b61f6e1c2c435ec5820683
boris-unckel <bu.githubcom@mail.unckel.net>
2021-12-18 20:58:20 +0100

    [4] Replace CVE-2017-5645 affected classes with NOP implementation
    Fixes https://github.com/boris-unckel/log4j1/issues/4
    Issue https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-5645

--- src/main/java/org/apache/log4j/net/SocketNode.java
+++ src/main/java/org/apache/log4j/net/SocketNode.java
@@ -17,30 +17,16 @@
 
 package org.apache.log4j.net;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggerRepository;
-import org.apache.log4j.spi.LoggingEvent;
-
-// Contributors:  Moses Hohman <mmhohman@rainbow.uchicago.edu>
 
 /**
-   Read {@link LoggingEvent} objects sent from a remote client using
-   Sockets (TCP). These logging events are logged according to local
-   policy, as if they were generated locally.
-
-   <p>For example, the socket node might decide to log events to a
-   local file and also resent them to a second socket node.
-
-    @author  Ceki G&uuml;lc&uuml;
-
-    @since 0.8.4
-*/
+ * No Operations. Avoid CVE-2017-5645
+ * https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-5645
+ **/
 public class SocketNode implements Runnable {
 
   Socket socket;
@@ -50,75 +36,10 @@ public class SocketNode implements Runnable {
   static Logger logger = Logger.getLogger(SocketNode.class);
 
   public SocketNode(Socket socket, LoggerRepository hierarchy) {
-    this.socket = socket;
-    this.hierarchy = hierarchy;
-    try {
-      ois = new ObjectInputStream(
-                         new BufferedInputStream(socket.getInputStream()));
-    } catch(InterruptedIOException e) {
-      Thread.currentThread().interrupt();
-      logger.error("Could not open ObjectInputStream to "+socket, e);
-    } catch(IOException e) {
-      logger.error("Could not open ObjectInputStream to "+socket, e);
-    } catch(RuntimeException e) {
-      logger.error("Could not open ObjectInputStream to "+socket, e);
-    }
+    //NOP
   }
 
-  //public
-  //void finalize() {
-  //System.err.println("-------------------------Finalize called");
-  // System.err.flush();
-  //}
-
   public void run() {
-    LoggingEvent event;
-    Logger remoteLogger;
-
-    try {
-      if (ois != null) {
-          while(true) {
-	        // read an event from the wire
-	        event = (LoggingEvent) ois.readObject();
-	        // get a logger from the hierarchy. The name of the logger is taken to be the name contained in the event.
-	        remoteLogger = hierarchy.getLogger(event.getLoggerName());
-	        //event.logger = remoteLogger;
-	        // apply the logger-level filter
-	        if(event.getLevel().isGreaterOrEqual(remoteLogger.getEffectiveLevel())) {
-	        // finally log the event as if was generated locally
-	        remoteLogger.callAppenders(event);
-	      }
-        }
-      }
-    } catch(java.io.EOFException e) {
-      logger.info("Caught java.io.EOFException closing conneciton.");
-    } catch(java.net.SocketException e) {
-      logger.info("Caught java.net.SocketException closing conneciton.");
-    } catch(InterruptedIOException e) {
-      Thread.currentThread().interrupt();
-      logger.info("Caught java.io.InterruptedIOException: "+e);
-      logger.info("Closing connection.");
-    } catch(IOException e) {
-      logger.info("Caught java.io.IOException: "+e);
-      logger.info("Closing connection.");
-    } catch(Exception e) {
-      logger.error("Unexpected exception. Closing conneciton.", e);
-    } finally {
-      if (ois != null) {
-         try {
-            ois.close();
-         } catch(Exception e) {
-            logger.info("Could not close connection.", e);
-         }
-      }
-      if (socket != null) {
-        try {
-          socket.close();
-        } catch(InterruptedIOException e) {
-            Thread.currentThread().interrupt();
-        } catch(IOException ex) {
-        }
-      }
-    }
+    //NOP
   }
 }
